d65302dc26efa695b29406df1ae8e6b0a18cd65c
Luis Sandoval <lsandovalancco@gmail.com>
2022-04-10 15:32:44 -0500

    Se agrega el controlador, y sus respectivas rutas

---
+++ src/main/java/com/ryg/market/web/controller/ProductController.java
@@ -0,0 +1,48 @@
+package com.ryg.market.web.controller;
+
+import com.ryg.market.domain.Product;
+import com.ryg.market.domain.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    /** Atributos */
+    @Autowired
+    private ProductService productService;
+
+    /** Metodos */
+
+    @GetMapping("/all")
+    public List<Product> getAll(){
+        return  productService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Product> getProduct(@PathVariable("id") int productId){
+        return productService.getProduct(productId);
+    }
+
+    @GetMapping("/category/{categoryId}")
+    public Optional<List<Product>> getByCategory(@PathVariable("categoryId") int categoryId){
+        return productService.getByCategory(categoryId);
+    }
+
+    @PostMapping("/save")
+    public Product save(@RequestBody Product product){
+        return productService.save(product);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public boolean delete(@PathVariable("id") int productId){
+        return productService.delete(productId);
+    }
+
+
+
+}
