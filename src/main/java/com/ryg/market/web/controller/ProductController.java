265c6962c7cb6793b2d232bd1ad9b614a5213aec
lsandoval <lsandoval@rygsol.com>
2022-04-11 19:07:46 -0500

    Se agrego el uso del response entity y http status al controlador.

--- src/main/java/com/ryg/market/web/controller/ProductController.java
+++ src/main/java/com/ryg/market/web/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.ryg.market.web.controller;
 import com.ryg.market.domain.Product;
 import com.ryg.market.domain.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,11 +14,64 @@ import java.util.Optional;
 @RequestMapping("/products")
 public class ProductController {
 
+    // Uso de response entity y http status
+    /*
+    *   El response entity, me permite generar respuestas http mas adecuadas
+    *   para los distintos tipos de casos. Se aprovechan todos los tipos de respuestas
+    *   que el protocolo http ofrece.
+    *
+    *   ResponseEntity(Body, Status HTTP)
+    *
+    * */
+
+
     /** Atributos */
     @Autowired
     private ProductService productService;
 
     /** Metodos */
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Product>> getAll(){
+        return  new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
+    }
+
+    // Agregamos la respuesta http NOT Found cuando un producto no se encuentra
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable("id") int productId){
+        return productService.getProduct(productId)
+                .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+
+    @GetMapping("/category/{categoryId}")
+    public ResponseEntity<List<Product>> getByCategory(@PathVariable("categoryId") int categoryId){
+        return productService.getByCategory(categoryId)
+                .map(products -> new ResponseEntity<>(products, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Product> save(@RequestBody Product product){
+        return new ResponseEntity<>(productService.save(product), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity delete(@PathVariable("id") int productId){
+        if(productService.delete(productId)){
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    /*
+
+    Ejemplo de uso inicial
+
+    @Autowired
+    private ProductService productService;
 
     @GetMapping("/all")
     public List<Product> getAll(){
@@ -43,6 +98,6 @@ public class ProductController {
         return productService.delete(productId);
     }
 
-
+    */
 
 }
